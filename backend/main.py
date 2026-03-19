a21f11d3e5aee0a13a6753cbf5e468ad87267713
trhao7105 <hao.trananhhao71@hcmut.edu.vn>
2026-03-19 19:41:23 +0700

    update

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",   
         "http://localhost:3000",   
-        "http://127.0.0.1:5173",
+        "https://dbas2-1.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
